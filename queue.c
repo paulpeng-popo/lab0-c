53cf2468ff0db98b13e67bd345574945751ab127
paulpeng-popo <kmes1234@gmail.com>
2023-02-26 21:22:07 +0800

    Add early return in `q_insert_`

--- queue.c
+++ queue.c
@@ -41,6 +41,10 @@ void q_free(struct list_head *l)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+
     element_t *entry = malloc(sizeof(element_t));
     if (!entry) {
         return false;
@@ -58,6 +62,10 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+
     element_t *entry = malloc(sizeof(element_t));
     if (!entry) {
         return false;
